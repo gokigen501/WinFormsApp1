db22568cbeb0f37ea8551a547077f3b29b12f055
和地 浩一 <gokigen501@gmail.com>
2022-11-16 22:46:28 +0900

    変更3

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -31,7 +31,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(212, 74)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button２"
+        Me.Button1.Text = "Button3"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
